1904a6cd9ace6713b1f81f870e4f5c398d81ed44
Pouyani Armin <pouyaniarmin@gmail.com>
2024-12-04 11:22:20 +0330

    upload image

--- src/helpers/FileUploadHelper.php
+++ src/helpers/FileUploadHelper.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Uploads a single image file to the server.
+ * 
+ * @param array $fields The $_FILES array containing image file data.
+ * @return string The path of the uploaded file.
+ */
 function uploadImage($fields)
 {
     $imagename = $fields['name'];
@@ -10,7 +15,12 @@ function uploadImage($fields)
     }
     return $folder;
 }
-
+/**
+ * Uploads images embedded in the HTML content (base64 encoded) to the server.
+ * 
+ * @param string $content The HTML content containing base64-encoded images.
+ * @return string The modified HTML content with image src paths updated.
+ */
 function uploadImagesFromContent($content)
 {
     $content = html_entity_decode($content);
@@ -20,13 +30,15 @@ function uploadImagesFromContent($content)
     $xpath = new DOMXPath($doc);
     $images = $xpath->query('//img');
     foreach ($images as $img) {
-        $src = $img->getAttribute('src');
+        if ($img instanceof DOMElement) {
+            $src = $img->getAttribute('src');
         if (preg_match('/^data:image\/(\w+);base64,/', $src, $matches)) {
             $image_data = substr($src, strpos($src, ',') + 1);
             $image_data = base64_decode($image_data);
             $filename = "uploads/" . uniqid() . '.' . $matches[1];
             file_put_contents($filename, $image_data);
             echo "Image uploaded to: " . $filename . "<br>";
+        }
         }
     }
     return $doc->saveHTML();
